1b5caf3b47aac1b12b8015caf390be15edb80b36
axxuxy <axxuxy@gmail.com>
2023-12-26 22:46:56 +0800

    Update pm2 config file.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,18 @@
-const fs = require("fs");
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
 
 module.exports = {
   apps: [
     {
       name: "ax-blog",
-      script: "./.output/server/index.mjs",
+      script: "server/index.mjs",
+      cwd: resolve(__dirname, ".output"),
       port: 443,
       exec_mode: "cluster",
       instances: "max",
       env: {
-        NITRO_SSL_CERT: fs.readFileSync("key/axxu.xyz.pem", "utf-8"),
-        NITRO_SSL_KEY: fs.readFileSync("key/axxu.xyz.key", "utf-8"),
+        NITRO_SSL_CERT: readFileSync(resolve(__dirname, "key/axxu.xyz.pem"), "utf-8"),
+        NITRO_SSL_KEY: readFileSync(resolve(__dirname, "key/axxu.xyz.key"), "utf-8"),
       }
     }
   ]
