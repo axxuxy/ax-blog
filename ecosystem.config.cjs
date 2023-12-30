9b16492df6034400e28e8dc01360edb350ac14b6
axxuxy <axxuxy@gmail.com>
2023-12-31 06:27:22 +0800

    Update pm2 config to 433 port.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,14 +7,13 @@ module.exports = {
       name: "ax-blog",
       script: "server/index.mjs",
       cwd: resolve(__dirname, ".output"),
-      // port: 443,
-      port: 80,
+      port: 443,
       exec_mode: "cluster",
       instances: "max",
-      // env: {
-      //   NITRO_SSL_CERT: readFileSync(resolve(__dirname, "key/axxu.xyz.pem"), "utf-8"),
-      //   NITRO_SSL_KEY: readFileSync(resolve(__dirname, "key/axxu.xyz.key"), "utf-8"),
-      // }
+      env: {
+        NITRO_SSL_CERT: readFileSync(resolve(__dirname, "key/axxu.xyz.pem"), "utf-8"),
+        NITRO_SSL_KEY: readFileSync(resolve(__dirname, "key/axxu.xyz.key"), "utf-8"),
+      }
     }
   ]
 }
