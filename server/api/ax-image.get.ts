74cd6c35de24791f2c211effde62d4dcc0c7ccac
ax许 <41471592+axxuxy@users.noreply.github.com>
2024-01-01 01:49:40 +0800

    Set download ax-image.exe file size. (#2)

--- server/api/ax-image.get.ts
+++ server/api/ax-image.get.ts
@@ -1,11 +1,12 @@
-import { existsSync, createReadStream } from "fs";
+import { existsSync, createReadStream, statSync } from "fs";
 import { axImagePath } from "~/server/api/ax-image.post";
 
 export default defineEventHandler(async (event) => {
   if (existsSync(axImagePath)) {
     setHeaders(event, {
       "Content-type": "application/octet-stream",
-      "Content-Disposition": "attachment; filename=ax-image.exe"
+      "Content-Disposition": "attachment; filename=ax-image.exe",
+      "Content-Length": statSync(axImagePath).size
     });
     return createReadStream(axImagePath);
   } else throw createError({
